9fde137543730429350ad9bf946569d195a067d0
Derek Hall <derek@tribehr.com>
2015-04-20 16:28:58 -0400

    Update config to point to TribeHR sandbox env.

--- config.php
+++ config.php
@@ -6,8 +6,12 @@ date_default_timezone_set('UTC');
 define('INTEGRATION_ID', '[put your integration ID here]');
 define('SECRET_SHARED_KEY', '[put your secret shared key here]');
 
-// Endpoint URL. Re-point locally for testing if desired.
-define('TRIBEHR_LOOKUP_API_ENDPOINT', 'https://app.tribehr.com/lookup/');
+// Lookup API endpoint to connect to.
+// Ensure that you're pointing to either the production or sandbox environment as appropriate and matches your
+//  integration ID and shared key.
+// You can also mock out the TribeHR system and point to your own solution for testing
+define('TRIBEHR_LOOKUP_API_ENDPOINT', 'https://app.sandbox.tribehr.com/lookup/');		// development sandbox
+// define('TRIBEHR_LOOKUP_API_ENDPOINT', 'https://app.tribehr.com/lookup/');			// production environment
 
 // Values that can be useful for debugging/testing an integration
 define('REQUEST_LOGGING_ENABLED', false);
